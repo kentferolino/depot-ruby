d217e154df83ff00338c618da763c732944ea781
Kent Ferolino <kentfero@gmail.com>
2021-04-09 11:16:02 +0800

    Able to add Line Items to cart.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,7 +17,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create product" do
-    assert_difference('Product.count') do
+    assert_difference('Product.count', 1) do
 
       post products_url, params: { 
         product: { 
@@ -52,6 +52,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
       } 
     }
     assert_redirected_to product_url(@product)
+  end
+
+
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+        delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
